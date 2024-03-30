5c59f373bf659b6881db019967e1376ba6a73b82
Casey Morris <morrisacasey@gmail.com>
2024-03-29 22:53:13 -0400

    test bucketlist page

--- netlify/functions/retrieve-bucketlists/retrieve-bucketlists.js
+++ netlify/functions/retrieve-bucketlists/retrieve-bucketlists.js
@@ -1,47 +1,26 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config()
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
 
-let uri = process.env.MONGODB_URI;
-let dbName = process.env.MONGODB_DATABASE;
-let collectionName = process.env.MONGODB_COLLECTION;
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+const clientPromise = mongoClient.connect();
 
-let cachedDb = null;
-
-async function connectToDatabase() {
-  if (cachedDb && cachedDb.serverConfig.isConnected()) {
-    console.log('Using cached database instance');
-    return cachedDb;
-  }
-
-  const client = await MongoClient.connect(uri);
-  const db = client.db(dbName);
-  
-  cachedDb = db;
-  return db;
+const handler = async (event) => {
+    try {
+        const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+        const collection = database.collection(process.env.MONGODB_COLLECTION);
+        const bucketLists = await collection.find({}).toArray();
+        return {
+            statusCode: 200,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(bucketLists)
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ Error: 'Failed to retrieve Bucket Lists.' })
+        };
+    }
 }
 
-exports.handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  try {
-    const db = await connectToDatabase();
-    const collection = db.collection(collectionName);
-    const bucketLists = await collection.find({}).limit(10).toArray();
-
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*' // Adjust according to your CORS policy
-      },
-      body: JSON.stringify(bucketLists)
-    };
-    
-  } catch (error) {
-    console.error('An error occurred while connecting to MongoDB', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Failed to connect to the database' })
-    };
-  }
-};
+module.exports = { handler };
